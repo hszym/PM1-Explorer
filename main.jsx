04d1b27ef2a609ce57af8834317a0888ac205c3d
hszym <45387296+hszym@users.noreply.github.com>
2026-03-25 17:34:58 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import PM1Explorer from './pm1-explorer.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PM1Explorer />
-  </React.StrictMode>,
-)
